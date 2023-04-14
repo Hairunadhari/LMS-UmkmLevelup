780d0450136631dc1ad5dfe6d48f4d90fbd7e0eb
arifin.twospaces <arifin.twospaces@gmail.com>
2023-04-14 14:18:18 +0700

    updt

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
+
+
+class HomeController extends Controller
+{
+    public function index(Request $request  ){
+        $d = [];
+        $currenturl = url()->full();
+        if (Auth::user()) {
+            $request->session()->put('url', $currenturl);
+        }else{
+            redirect('login');
+        }
+        $check = DB::table('form_submissions')->where('id_user', Auth::user()->id)->count();
+        if($check == 0){
+            $d['done'] = false;
+        }else{
+            $d['done'] = true;
+        }
+        $d['dataProv'] = DB::table('m_provinsi')->where('aktif', 1)->get();
+        $forms = DB::table('forms')->whereNull('deleted_at')->orderBy('id', 'DESC')->first();
+        $d['data'][0]['link'] = config('app.url').'/kuesioner?href='.env('KUISIONER_URL').'/forms/'.$forms->slug.'/'.Auth::user()->id;
+        $d['data'][0]['title'] = $forms->title;
+        $d['data'][0]['desc'] = $forms->description;
+
+        return view('home', $d);
+    }
+
+    public function profil(){
+        $d = [];
+        $d['user'] = DB::table('profil_user')->where('id_user', Auth::user()->id)->first();
+        $d['nama_kabupaten'] = DB::table('m_kabupaten')->where('id_kabupaten', $d['user']->id_kabupaten)->first()->nama_kabupaten;
+        $d['nama_kecamatan'] = DB::table('m_kecamatan')->where('id_kecamatan', $d['user']->id_kecamatan)->first()->nama_kecamatan;
+        $d['nama_kelurahan'] = DB::table('m_kelurahan')->where('id_kelurahan', $d['user']->id_keluarahan)->first()->nama_kelurahan;
+        $forms = DB::table('forms')->orderBy('id', 'DESC')->first();
+        $d['dataProv'] = DB::table('m_provinsi')->where('aktif', 1)->get();
+        $d['data'][0]['link'] = config('app.url').'/kuesioner?href='.env('KUISIONER_URL').'/forms/'.$forms->slug.'/'.Auth::user()->id;
+        $d['data'][0]['title'] = 'Test Kuesioner';
+        $d['data'][0]['desc'] = 'Desc Kuesioner';
+        return view('profil', $d);
+    }
+
+     public function kuesioner()
+    {
+        $check = DB::table('form_submissions')->where('id_user', Auth::user()->id)->count();
+        if($check == 0){
+            $d['done'] = false;
+        }else{
+            $d['done'] = true;
+        }
+        return view('kuesioner', $d);
+    }
+}
--- resources/views/kuesioner.blade.php
+++ resources/views/kuesioner.blade.php
@@ -1,41 +1,14 @@
 @extends('layout.main')
 
 @section('container')
-  <div
-    class="pt-5"
-    style="
-      background-image: url('./assets/background_regist_bawah.png');
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position-y: bottom;
-      height: 30vh;
-    "
-  >
+<div class="container-fluid mt-2" >
+  @if ($done == true)
+      <h4 style="color: red; text-align:center">Anda sudah mengisi kuesioner ini </h4><br>
+      <div class="text-center"><a class="" href="home">Kembali</a></div>
+  @else
+    <iframe style="border:none;width:100%; height: 75vh" onload="resizeIframe(this)" src="{{$_GET['href']}}"></iframe>
+  @endif
   
-    <div class="container">
-      <nav aria-label="breadcrumb">
-        <ol class="breadcrumb justify-content-end">
-          {{-- <li class="breadcrumb-item active">Registrasi</li> --}}
-          {{-- <li class="breadcrumb-item active" aria-current="page">
-            Step By Step
-          </li> --}}
-        </ol>
-      </nav>
-      <h2
-        class="text-uppercase text-center font-bold"
-        style="font-size: 3rem; font-weight: bolder"
-      >
-      Test Kuesioner
-      </h2>
-    </div>
-  </div>
-  <div class="container-fluid pt-5">
-    @if ($done == true)
-        <h4 style="color: red; text-align:center">Anda sudah mengisi kuesioner ini </h4><br>
-        <div class="text-center"><a class="" href="home">Kembali</a></div>
-    @else
-      <iframe style="border:none;width:100%;" height="620px" src="{{$_GET['href']}}"></iframe>
-    @endif
-    
-  </div>
+</div>
+ 
 @endsection
