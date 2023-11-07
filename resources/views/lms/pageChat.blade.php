67dbe507a95eed4b8232aa085a109b74ec3a461e
Yoli Alfitriani <hairunadhari@gmail.com>
2023-11-07 16:00:47 +0700

    up

--- resources/views/lms/pageChat.blade.php
+++ resources/views/lms/pageChat.blade.php
@@ -254,7 +254,7 @@
                         {{-- <input style="display: none" type="text"
                             value="/storage/data_upload_lms/HVETLEWJ1PLYq9rNbFsigQdIQGZqByvd6isMSqve.pdf"
                             id="file_location"> --}}
-                            <input style="display: none" type="text" value="{{$sub_materi->file_location}}"
+                            <input style="display: block" type="text" value="{{$sub_materi->file_location}}"
                             id="file_location">
                         <input style="display: none" type="text" value="{{$sub_materi->video_url}}" id="video_url">
                         <input style="display: none" type="text" value="{{$materiid}}" id="id_materi">
