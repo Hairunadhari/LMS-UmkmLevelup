bf382e566b1db9078a26b1742384a375e52cd8a6
Yoli Alfitriani <hairunadhari@gmail.com>
2024-01-23 15:35:10 +0700

    footer di email

--- resources/views/emails/demoMail.blade.php
+++ resources/views/emails/demoMail.blade.php
@@ -1,27 +1,60 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>UMKM Levelup.id</title>
 </head>
+
 <body>
     <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-        <div style="margin:50px auto;width:70%;padding:20px 0">
-          <div style="border-bottom:1px solid #eee">
-            <img src="{{asset('asset/logo.png')}}" alt="logo umkm levelup" /><br>
-            <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">UMKM Levelup</a>
-          </div>
-          <p style="font-size:1.1em">Hai,</p>
-          <p>Terimakasih  sudah melakukan registrasi. Berikut nomor OTP, silahkan masukkan nomor ini ke website kami :</p>
-          <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{{$mailData['otp']}}</h2>
-          <p style="font-size:0.9em;">Salam,<br />Admin UMKM Levelup</p>
-          <hr style="border:none;border-top:1px solid #eee" />
-          <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-            <p>UMKM Levelup</p>
-            {{-- <p>1600 Amphitheatre Parkway</p> --}}
-            {{-- <p>California</p> --}}
-          </div>
+        <div style="margin:0px auto;width:70%;padding:20px 0">
+            <div style="border-bottom:1px solid #eee">
+                <img src="https://umkmlevelup.id/asset/logo.png" alt="logo umkm levelup" /><br>
+                <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">UMKM Levelup</a>
+            </div>
+            <p style="font-size:1.1em">Hai,</p>
+            <p>Terimakasih sudah melakukan registrasi. Berikut nomor OTP, silahkan masukkan nomor ini ke website kami :
+            </p>
+            <h2
+                style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">
+                {{$mailData['otp']}}</h2>
+            <p style="font-size:0.9em;">Salam,<br />Admin UMKM Levelup</p>
+            <hr style="border:none;border-top:1px solid #eee" />
         </div>
-      </div>
+        <!-- Start footer -->
+        <table align="center"
+            style="text-align: center; vertical-align: top; width: 70%; max-width: 70%; background-color: #0C438D;"
+            width="600">
+            <tbody>
+                <tr>
+                    <td style="width: 596px; vertical-align: top; padding-left: 30px; padding-right: 30px; padding-top: 30px; padding-bottom: 30px;"
+                        width="596">
+
+                        <!-- Your inverted logo is here -->
+                        <img style=" text-align: center; color: #ffffff;" alt="Logo"
+                            src="https://umkmlevelup.id/asset/logo-kominfo.png" align="center">
+
+                        <p
+                            style="font-size: 13px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #ffffff;">
+                            Jl. Medan Merdeka Barat No. 9, Jakarta 10110 , tanya@umkmlevelup
+                        </p>
+
+                        <p
+                            style="margin-bottom: 0; font-size: 13px; line-height: 24px; font-family: 'Helvetica', Arial, sans-serif; font-weight: 400; text-decoration: none; color: #ffffff;">
+                            <a target="_blank" style="text-decoration: underline; color: #ffffff;"
+                                href="https://umkmlevelup.id">
+                                www.umkmlevelup.id
+                            </a>
+                        </p>
+
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- End footer -->
+
+    </div>
+
 </body>
+
 </html>
