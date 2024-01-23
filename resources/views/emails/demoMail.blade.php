e5542e71d2487a3a33bfbc5d182842ce1e122b38
arifin.twospaces <arifin.twospaces@gmail.com>
2024-01-23 10:13:44 +0700

    update

--- resources/views/emails/demoMail.blade.php
+++ resources/views/emails/demoMail.blade.php
@@ -8,7 +8,7 @@
     <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
         <div style="margin:50px auto;width:70%;padding:20px 0">
           <div style="border-bottom:1px solid #eee">
-            <img src="{{asset('asset/logo.png')}}" alt="logo umkm levelup" />
+            <img src="{{asset('asset/logo.png')}}" alt="logo umkm levelup" /><br>
             <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">UMKM Levelup</a>
           </div>
           <p style="font-size:1.1em">Hai,</p>
