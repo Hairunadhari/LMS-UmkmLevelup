662b23d52c4cc4fe23a75685d957d7c40c50dc9e
arifin.twospaces <arifin.twospaces@gmail.com>
2024-03-31 13:15:05 +0700

    update

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -80,8 +80,8 @@
           <div class="card h-100 p-4">
             <div class="card-body">
               <img src="{{url('/')}}/asset/umkm-level-up-business-incubator.png" alt="" class="img-fluid mb-2" width="700">
-              <h5 class="fw-semibold text-center">UMKM Level Up Business Akselerator</h5>
-              <p class="text-body-secondary">Pendampingan intensif kepada 100 UMKM melalui sesi One-on-One Business coaching oleh Dedicated Business Coach, serta online Mentoring untuk mendorong UMKM melakukan akselerasi bisnis.</p>
+              <h5 class="fw-semibold text-center">UMKM Level Up Business Accelerator</h5>
+              <p class="text-body-secondary">Pendampingan intensif kepada 100 UMKM melalui sesi One-on-One Business coaching oleh Dedicated Business Coach, serta online Mentoring untuk mendorong UMKM melakukan akselerasi bisnis</p>
               <div class="text-center">
                 <a class="btn btn-primary btn-md" href="https://akselerator.umkmlevelup.id/pendaftaran">Registrasi <i class="fa fa-arrow-right"></i></a>
               </div>
